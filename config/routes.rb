f1525cbeb2485a4c059d0a210c2e31b6e0889a29
gds1618 <guilhermduart@gmail.com>
2019-08-20 17:46:01 -0300

    work crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'articles#index'
+  # get 'articles/new'
+  # get 'articles/create'
+  # get 'articles/index'
+  # get 'articles/show'
+  # get 'articles/edit'
+  # get 'articles/update'
+  # get 'articles/destroy'
+  resources :articles
 end
